a095bc61f5017362c0c19caea590e32a5db0c491
Brandon H <brandon@bc3tech.net>
2017-06-02 11:03:33 -0700

    v1.10.862.6

--- vivaldi/tools/chocolateyinstall.ps1
+++ vivaldi/tools/chocolateyinstall.ps1
@@ -1,19 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= 'vivaldi'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = 'vivaldi'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
 $packageArgs = @{
-  packageName   = $packageName
-  FileFullPath  = Join-Path $toolsDir 'vivaldi-downloaded.zip'
+    packageName    = $packageName
+    FileFullPath   = Join-Path $toolsDir 'vivaldi-downloaded.zip'
  
-  url           = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.10.856.16.exe'
-  checksum      = '16B095F636CAC681E3F1C90079964F324383EABC223001BA3DBBA08A5E7589AE'
-  checksumType  = 'sha256' #default is md5, can also be sha1
+    url            = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.10.862.6.exe'
+    checksum       = ''
+    checksumType   = 'sha256' #default is md5, can also be sha1
  
-  url64bit      = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.10.856.16.x64.exe'
-  checksum64    = '42A8CFB79E5459CA4435470BBCC1060073DB3F0C4DA5C68ADE0317AD740AE03B'
-  checksumType64= 'sha256' #default is md5, can also be sha1
+    url64bit       = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.10.862.6.x64.exe'
+    checksum64     = 'B4393D97FEBB6970C5BC5298D6B00EFDA58A5BF996262A1DFB1C7DF99423C46A'
+    checksumType64 = 'sha256' #default is md5, can also be sha1
 }
  
 $downloadedFile = Get-ChocolateyWebFile @packageArgs
