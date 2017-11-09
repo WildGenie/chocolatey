c0f5a3b6032de84e8bbbe11d9546e8e0e1314188
Brandon H <brandon@bc3tech.net>
2017-11-09 09:11:37 -0800

    v1.13.1008.11 + usage of silent installer instead of unzipping!

--- vivaldi/tools/chocolateyinstall.ps1
+++ vivaldi/tools/chocolateyinstall.ps1
@@ -5,24 +5,19 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
 $packageArgs = @{
     packageName    = $packageName
-    FileFullPath   = Join-Path $toolsDir 'vivaldi-downloaded.zip'
- 
-    url            = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.13.1008.3.exe' # NEXT TIME DO SILENT INSTALL SETUP!!!!!
-    checksum       = 'A62C5731B4100FE88E397F88C8B26F71FF4FB6D0E8961DD5C4D47443AD53688B'
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+       
+    url            = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.13.1008.11.exe'
+    silentArgs     = '--vivaldi-silent --do-not-launch-chrome --vivaldi-update'
+    
+    checksum       = 'FA1EF6AA7F2F8584A01E3B8A6CAA741F33F20DD764551B883E66578DA048AD19'
     checksumType   = 'sha256' #default is md5, can also be sha1
- 
-    url64bit       = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.13.1008.3.x64.exe'
-    checksum64     = '4002667A70238316F2D5268D4EBB9714EB1CDC1803CD7225F3AD604ADE241656'
+    url64bit       = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.13.1008.11.x64.exe'
+    checksum64     = '3B72938D47765BD9A13636C0F5052515EC9D1CF6CC1803798392204A36F0DEF2'
     checksumType64 = 'sha256' #default is md5, can also be sha1
 }
- 
-$downloadedFile = Get-ChocolateyWebFile @packageArgs
- 
-Write-Host 'Unpacking vivaldi installer like an archive...'
-& 7z x -aoa -bb0 -bso0 -o"$($toolsDir)\$($packageName)" $downloadedFile
-Get-ChocolateyUnzip "$($toolsDir)\$($packageName)\vivaldi.7z" "$($toolsDir)\$($packageName)"
- 
-Write-Host 'Creating *.ignore files to avoid making shims for other exes'
-New-Item "$($toolsDir)\$($packageName)\Vivaldi-bin\crash_service.exe.ignore" -type file -force | Out-Null
-New-Item "$($toolsDir)\$($packageName)\Vivaldi-bin\wow_helper.exe.ignore" -type file -force | Out-Null
-New-Item "$($toolsDir)\$($packageName)\Vivaldi-bin\update_notifier.exe.ignore" -type file -force | Out-Null
+
+Install-ChocolateyPackage @packageArgs
+
+Write-Host 'Vivaldi has been installed with a silent installer. Any shortcuts you had will need to be re-generated.' # REMOVE NEXT TIME
