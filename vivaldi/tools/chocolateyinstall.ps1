fd0df1f7eaf402503eb00171477ff56d3b4e9494
Brandon H <brandon@bc3tech.net>
2017-11-29 12:24:02 -0800

    v1.14.1030.3

--- vivaldi/tools/chocolateyinstall.ps1
+++ vivaldi/tools/chocolateyinstall.ps1
@@ -8,13 +8,13 @@ $packageArgs = @{
     unzipLocation  = $toolsDir
     fileType       = 'exe'
        
-    url            = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.13.1008.30.exe'
+    url            = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.14.1030.3.exe'
     silentArgs     = '--vivaldi-silent --do-not-launch-chrome --vivaldi-update'
     
-    checksum       = '5F399772C7B2A544820E9DEF21208EF595480BC37FF0DDAD9155D952408F9CE7'
+    checksum       = 'CF8E24C3F996024BDCB2A1C7F645D9FD57EEA0E1279BFEE399C96CCF099E8DDB'
     checksumType   = 'sha256' #default is md5, can also be sha1
-    url64bit       = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.13.1008.30.x64.exe'
-    checksum64     = '970B34931A84CC4DECB3FA5CBC1C6783853CA53CF0232C6078E9839A7FB55FEA'
+    url64bit       = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.14.1030.3.x64.exe'
+    checksum64     = '5E6D11579F19BB499F3E0F9B024405D10E1E29159CC213F7B0DC4697804EE6DC'
     checksumType64 = 'sha256' #default is md5, can also be sha1
 }
 
