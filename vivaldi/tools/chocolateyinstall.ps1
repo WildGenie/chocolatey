0718d7e4b8a3d073cb70bfc06a5b851eb7d61d81
Brandon H <brandon@bc3tech.net>
2017-12-07 09:16:16 -0500

    v1.14.1036.3

--- vivaldi/tools/chocolateyinstall.ps1
+++ vivaldi/tools/chocolateyinstall.ps1
@@ -8,16 +8,14 @@ $packageArgs = @{
     unzipLocation  = $toolsDir
     fileType       = 'exe'
        
-    url            = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.14.1030.3.exe'
+    url            = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.14.1036.3.exe'
     silentArgs     = '--vivaldi-silent --do-not-launch-chrome --vivaldi-update'
     
-    checksum       = 'CF8E24C3F996024BDCB2A1C7F645D9FD57EEA0E1279BFEE399C96CCF099E8DDB'
+    checksum       = '1CC8AA0110F0B3A1B2E52AE062F55E73537585A9B20E0BDADCC995E42CDA5ACC'
     checksumType   = 'sha256' #default is md5, can also be sha1
-    url64bit       = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.14.1030.3.x64.exe'
-    checksum64     = '5E6D11579F19BB499F3E0F9B024405D10E1E29159CC213F7B0DC4697804EE6DC'
+    url64bit       = 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.14.1036.3.x64.exe'
+    checksum64     = 'A5B2762F5F587B9878E05998976146DDB36CB6A08622AF27D3A80F4145F2B4AB'
     checksumType64 = 'sha256' #default is md5, can also be sha1
 }
 
 Install-ChocolateyPackage @packageArgs
-
-Write-Host "Vivaldi has been installed with a silent installer. Any shortcuts you had to the previous portable installer's location will need to be re-generated." # REMOVE NEXT TIME
